99e65183ddcc30ba3af470167be4359bf74a9ff7
Felix Vaughan <ezamabusiness@gmail.com>
2020-12-31 13:48:14 -0700

    add incomplete File class and search methods

--- mfs.py
+++ mfs.py
@@ -1,7 +1,52 @@
-from PyPDF2 import PdfFileReader
 import slate3k as PDFReader
 from os.path import splitext
 import random
+
+ACCEPTABLE_SUFFIXES = ["pdf","txt","c","cpp","py","pyx","java"] #this list can be extended to include any type of code file. These are not all included for obvious reasons
+def checkSuffix(suffix):
+    if suffix not in ACCEPTABLE_SUFFIXES:
+        return False
+    else:
+        return True
+
+def findOccurences(key,word,wordStore,cycle):
+    index = 0
+    while index < len(word):
+            index = word.find(key, index)
+            if index == -1:
+                break
+            if cycle < 2:
+                wordStore.append(cycle + index)
+            else:
+                wordStore.append(cycle + index - 3)
+            index += len(key)
+
+def readPDF(stream, keyword):
+    with open(stream,"rb") as file:
+        pdf = PDFReader.PDF(file)
+        occurences = []
+        for content in pdf:
+            print(content)
+            findOccurences(keyword,content,occurences,0)
+        print(len(occurences))
+
+def readText(file, keyword):
+    absKeyLength = len(keyword) - 1
+    file = input("File to search: ")
+    with open(file, encoding='utf8') as f:
+        carryOver = ""
+        cycle = 0
+        occurences = []
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+
+            data = carryOver + data
+            findOccurences(keyword,data,occurences,cycle)
+            suffix = data[len(data)-absKeyLength - 1 : len(data)]
+            carryOver = cycle + 1024
+        print(occurences)
 
 class Color:
     PURPLE = '\033[95m'
@@ -43,3 +88,23 @@ class WordManager:
 
     def addOccurrence(self,page,row):
         self.occurences.append((page,row))
+
+class File:
+    def __init__(self,path):
+        self.path = path
+        try:
+            if checkSuffix(self.getSuffix()) == False:
+                raise ValueError
+            self.file = open(path,"r")
+        except ValueError:
+            print(f"'{path}' does not contain a valid suffix, skipping...") #make sure to add skip clause in main loop
+        except FileNotFoundError:
+            print(f"No file found at '{path}', skipping...")   #make sure to add skip clause in main loop
+
+    def getSuffix(self):
+        reverse_path = self.path[::-1]
+        suffix_index = reverse_path.index(".")
+        reverse_suffix = reverse_path[0:suffix_index]
+        return reverse_suffix[::-1]
+
+file = File("/Users/Felix Vaughan/Desktop/toAlex.txt")
